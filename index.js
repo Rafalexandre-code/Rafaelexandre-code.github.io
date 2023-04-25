b1eecff4ad6c33be71ab2c363d831e0fde78b912
Rafalexandre-code <rafaelalexandre9816@gmail.com>
2023-04-25 14:20:24 -0300

    altera

--- index.js
+++ index.js
@@ -1,83 +1,100 @@
 //abre o servidor na porta 9090
 const http = require("http");
-const websocketServer = require("websocket").server
-const httpServer = http.createServer();
-httpServer.listen(9090, () => console.log("Servidor escutando na porta 9090"))
+
+const express = require("express");
+
+const webServerApp = express();
+
+const websocketServer = require("websocket").server;
+const httpServer = http.createServer(webServerApp);
+httpServer.listen(9090, () => console.log("Servidor escutando na porta 9090"));
+
+webServerApp.get("/", function (req, res) {
+
+  res.sendFile(__dirname + "/server.html");
+});
 
 //abre o cliente na porta 9091
 const app = require("express")();
-app.get("/", (req,res)=> res.sendFile(__dirname + "/index.html"))
-app.listen(9091, ()=>console.log("Cliente escutando na porta 9091"))
-
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+app.listen(9091, () => console.log("Cliente escutando na porta 9091"));
 
 //cliente armazenado por hashmap
 const clientes = {};
-const jogo={};
+const jogo = {};
 
 const Server = new websocketServer({
-    "httpServer": httpServer
-})
-Server.on("request", pedido => {
-    //conecxão
-    const conexão = pedido.accept(null, pedido.origin);
-    conexão.on("open", () => console.log("Usuário conectado!"))
-    conexão.on("close", () => console.log("Usuário desconectado!"))
-    //recebe as mensagens do usuario
-    conexão.on("message", mensagem => {
-        const resultado = JSON.parse(mensagem.utf8Data)
+  httpServer: httpServer,
+});
+Server.on("request", (pedido) => {
+  //conecxão
+  console.log("alo!");
+  const conexao = pedido.accept(null, pedido.origin);
+  conexao.on("open", () => console.log("Usuário conectado!"));
+  conexao.on("close", () => console.log("Usuário desconectado!"));
+  //recebe as mensagens do usuario
+  conexao.on("message", (mensagem) => {
+    const resultado = JSON.parse(mensagem.utf8Data);
 
+    //onde recebe a mensagem do usuario
+    if (resultado.methodo == "novojogo") {
+      clientes[resultado.idcliente] = {
+        conexao: conexao,
+      };
+      console.log(resultado);
+      console.log("bla");
+      const idjogo = guid();
+      jogo[idjogo] = {
+        id: idjogo,
+        nave: 1,
+      };
 
-        //onde recebe a mensagem do usuario
-        if(resultado.methodo == "novojogo"){
-          const idjogo = guid();
-          jogo[idjogo] = {
-            "id": idjogo,
-            "nave": 1
-          }
+      const payLoad = {
+        methodo: "novojogo",
+        jogo: jogo[idjogo],
+      };
 
-          const payLoad = {
-            "methodo": "novojogo",
-            "jogo": jogo[idjogo]
-          }
-
-          const con = clientes[idcliente].conexão;
-          con.send(JSON.stringify(payLoad))
-        }
-        
-        if(resultado.methodo == "jogando"){
-          
-        }
-    })
-
-
-    //gera um id de cliente
-    const idcliente = guid();
-    clientes[idcliente] = {
-        "conexão":  conexão
+      /*const con = clientes[idcliente].conexao;
+      console.log(clientes);
+      con.send(JSON.stringify(payLoad));*/
+      
+      console.log(clientes);
     }
 
-    const payLoad = {
-        "methodo": "connect",
-        "idcliente": idcliente
+    if (resultado.methodo == "jogando") {
     }
-    //manda de volta a conexão do cliente
-    conexão.send(JSON.stringify(payLoad))
+  });
 
-})
+  //gera um id de cliente
+  /*const idcliente = guid();
 
+  const payLoad = {
+    methodo: "connect",
+    idcliente: idcliente,
+  };
+  //manda de volta a conexao do cliente
+  conexao.send(JSON.stringify(payLoad));*/
+});
 
 //função para id de usuario
 function guid() {
-    return ((((1+Math.random())*0x10000)|0).toString(16).substring(1) + (((1+Math.random())*0x10000)|0).toString(16).substring(1) + "-" + 
-    (((1+Math.random())*0x10000)|0).toString(16).substring(1) + "-4" + (((1+Math.random())*0x10000)|0).toString(16).substring(1).substr(0,3) + "-" + 
-    (((1+Math.random())*0x10000)|0).toString(16).substring(1) + "-" + (((1+Math.random())*0x10000)|0).toString(16).substring(1) + 
-    (((1+Math.random())*0x10000)|0).toString(16).substring(1) + (((1+Math.random())*0x10000)|0).toString(16).substring(1)).toLowerCase(); 
+  return (
+    (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1) +
+    (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1) +
+    "-" +
+    (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1) +
+    "-4" +
+    (((1 + Math.random()) * 0x10000) | 0)
+      .toString(16)
+      .substring(1)
+      .substr(0, 3) +
+    "-" +
+    (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1) +
+    "-" +
+    (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1) +
+    (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1) +
+    (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1)
+  ).toLowerCase();
 }
 
-
 //jogo
-
-
-
-
-
